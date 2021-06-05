be317089494cbf1f54f8753f71e50414494abbe2
PBonvang <peter.bonvang@gmail.com>
2021-06-05 19:57:54 +0200

    Warning/disclaimer added to selfContainingCK attribute;

--- src/Conditus.DynamoDB.MappingExtensions/Attributes/DynamoDBSelfContainingCompositeKeyAttribute.cs
+++ src/Conditus.DynamoDB.MappingExtensions/Attributes/DynamoDBSelfContainingCompositeKeyAttribute.cs
@@ -5,6 +5,11 @@ namespace Conditus.DynamoDB.MappingExtensions.Attributes
     /// <summary>
     /// This attribute specifies that the DynamoDB value of this attributes shall be a composite key
     /// consisting of the value of this property and the values of the composite keys passed in the constructor
+    /// 
+    /// Warnings:
+    ///   - As this key will use the values of other properties in the entity, 
+    ///     and that the IPropertyConverter only takes the value of the single property/attribute
+    ///     DynamoDBContext won't be able to convert from and to, forcing you to use the low level API
     /// </summary>
     public class DynamoDBSelfContainingCompositeKeyAttribute : Attribute
     {
